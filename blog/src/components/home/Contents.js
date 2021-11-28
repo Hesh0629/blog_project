6e26b7aedcaa655fc717b6006ea401e1929e741d
한성환 <hesh0629@kakao.com>
2021-11-28 14:55:24 +0000

    Edit Contents

--- blog/src/components/home/Contents.js
+++ blog/src/components/home/Contents.js
@@ -2,10 +2,16 @@ import React from 'react';
 import '../style/contents.scss';
 const Contents = () => {
 	return (
-		<div class="contents">
-			<h1>집짓기 1일차</h1>
-			<p>나도 내 집을 장만할테야 <br/>여기다간 내 소개를 하리라</p>
-		</div>
+		<>
+			<div class="contents">
+				<h1>집짓기 1회차</h1>
+				<p>나도 내 집을 장만할테야 <br/>여기다간 내 소개를 하리라</p>
+			</div>
+			<div class="contents">
+				<h1>집짓기 2회차</h1>
+				<p>헤더 만들기 완료<br/>미디어 쿼리 적용 X</p>
+			</div>
+		</>
 	);
 };
 export default Contents;
