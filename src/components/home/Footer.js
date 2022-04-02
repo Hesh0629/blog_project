e70d47229a470a9a14135534eb1a6f28fdc01bfe
HeSH <neogate@sogang.ac.kr>
2022-04-03 00:03:20 +0900

    edit footer

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
 		<>
 			<div class="footer">
 				<div class="text">
-					<div>Last update : 2022.04.02ss</div>
+					<div>Last update : 2022.04.02</div>
 					<div>How to Reach me : neogate@sogang.ac.kr</div>
 					<a class="copyright" href="https://www.vecteezy.com/free-vector/statistics-icon">Statistics Icon Vectors by Vecteezy</a>
 				</div>
