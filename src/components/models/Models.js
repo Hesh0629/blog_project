5ece27ed0ecc61ff8c26c47b3ac76c8a8792e4e6
HeSH <neogate@sogang.ac.kr>
2023-01-05 18:52:50 +0900

    fix width

--- src/components/models/Models.js
+++ src/components/models/Models.js
@@ -4,16 +4,18 @@ import '../style/models.scss';
 const Models = () => {
 	const effect = useScrollFadeIn('down');
 	return (
-		<div class="models">
-			<div {...effect}>
-				<div class="title">
-                    여기다가 지금까지 <br/>
-                    만든 모델 올릴거임
-                </div>
-				<div class="small-title">
-                    근데 아직 공부중임<br/>
-                    BentoML 언제하지
-                </div>
+		<div class="wrapper">
+			<div class="models">
+				<div {...effect}>
+					<div class="title">
+                    	여기다가 지금까지 <br/>
+                    	만든 모델 올릴거임
+                	</div>
+					<div class="small-title">
+                    	근데 아직 공부중임<br/>
+                    	BentoML 언제하지
+                	</div>
+				</div>
 			</div>
 		</div>
 	);
